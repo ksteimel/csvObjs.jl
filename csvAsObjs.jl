f7e7923b5fd756aaf6ed72f2be6fe056479aba2c
kenneth <kenneth@tani.ksteimel.duckdns.org>
2017-08-11 11:45:29 -0400

    beginning to work on delimiter inferrence

--- csvAsObjs.jl
+++ csvAsObjs.jl
@@ -1,18 +1,36 @@
 using ArgParse
+function inferDelim(sampleLines)
+
+	return delim
+end
 function main(args)
 	#initialize settings for argparse library
 	s = ArgParseSettings(description = "Script to create live objects from rows in csv")
 	@add_arg_table opts begin
-		"--delimiter", "-d" #specifies the delimiter for the csv. If none is specified, one is inferred
-		"file", "--file" #argument for the csv file to read in. Can be either positional or indicated using a flag
-
+		"--delimiter", "-d" 
+			help = "Specifies the delimiter for the csv. If none is specified, one is inferred"
+		"file", "--file" 
+			help = "Argument for the csv file to read in. Can be either positional or indicated using a flag"
+			required = true
 	end
+	parsed_args = parse_args(s)
 	#Read first line of csv in so we can figure out what the headings should be
 	if length(ARGS) > 1
 		open(ARGS[1]) do csvInput 
-			s = readstring(csvInput)
-			header = s[1]
-			
+			src = readlines(csvInput)
+			header = src[1]
+			if(haskey(parsed_args, "--delimiter"))
+				delim = parsed_args["--delimiter"]
+			elseif(haskey(parsed_args, "-d"))
+				delim = parsed_args["-d"]
+			else
+				lenFile = length(src)
+				if(lenFile > 10)
+					sampleLines = src[1:5:end]
+				else
+					sampleLines = src[:]
+				end	
+			end	
 		end
 	end
 end
